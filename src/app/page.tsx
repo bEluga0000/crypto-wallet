0a569fef2cbe7c72ea816e433466b22de15f2266
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-25 01:01:28 +0530

    feat: introduce home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HomePage from "@/components/home";
 
 export default function Home() {
   return (
     <div>
-      Crypto - Wallet
+      <HomePage/>
     </div>
   );
 }
