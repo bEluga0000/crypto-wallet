f7342cf900af003bd36e1b80c345aa461b37e1de
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-25 01:24:40 +0530

    feat: create a two new components for the homepage

---
+++ src/components/ui/home/button.tsx
@@ -0,0 +1,20 @@
+interface ButtonForHomePageProps {
+    icon?: React.ReactNode
+    title: string
+    onClick: () => void
+}
+const ButtonForHomePage: React.FC<ButtonForHomePageProps> = ({
+    icon,
+    title,
+    onClick
+}) => {
+    return <button className="mt-4 flex h-14 w-full items-center justify-center gap-2 rounded-lg bg-primary text-lg font-bold text-white transition-transform group-hover:scale-[1.02]" onClick={onClick}>
+        {title}
+        {
+            icon && <span className="material-symbols-outlined">
+                {icon}
+            </span>
+        }
+    </button>
+}
+export default ButtonForHomePage
--- src/components/ui/home/homePageCard.tsx
+++ src/components/ui/home/homePageCard.tsx
@@ -1,24 +1,44 @@
-const HomePageCard = () => {
+import ButtonForHomePage from "./button";
+
+interface HomePageCardProps {
+    icon?: React.ReactNode;
+    title: string
+    desc?: string
+    button?: string
+    onClick?: () => void
+    buttonIcon?:React.ReactNode
+}
+
+const HomePageCard: React.FC<HomePageCardProps> = ({
+    icon,
+    title,
+    desc,
+    button,
+    onClick,
+    buttonIcon
+}) => {
     return <div className="group flex cursor-pointer flex-col gap-6 rounded-xl border border-[#e5e7eb] bg-white p-8 shadow-sm transition-all hover:border-primary hover:shadow-xl dark:border-gray-800 dark:bg-[#1a212f]">
-        <div className="flex size-14 items-center justify-center rounded-xl bg-primary text-white shadow-lg shadow-primary/20">
-            <span className="material-symbols-outlined text-[32px]">
-                add_moderator
-            </span>
-        </div>
+        {
+            icon && <div className="flex size-14 items-center justify-center rounded-xl bg-primary text-white shadow-lg shadow-primary/20">
+                <span className="material-symbols-outlined text-[32px]">
+                    {icon}
+                </span>
+            </div>
+        }
+
 
         <div>
-            <h3 className="text-2xl font-bold">Create New Wallet</h3>
-            <p className="mt-2 text-[#616f89] dark:text-gray-400">
-                Generate a new 24-word recovery phrase for a fresh start.
-            </p>
+            <h3 className="text-2xl font-bold">{title}</h3>
+            {
+                desc && <p className="mt-2 text-[#616f89] dark:text-gray-400">
+                    {desc}
+                </p>
+            }
         </div>
 
-        <button className="mt-4 flex h-14 w-full items-center justify-center gap-2 rounded-lg bg-primary text-lg font-bold text-white transition-transform group-hover:scale-[1.02]">
-            Get Started
-            <span className="material-symbols-outlined">
-                arrow_forward
-            </span>
-        </button>
+        {
+            button && onClick && <ButtonForHomePage title={button} onClick={onClick} icon={buttonIcon}/>
+        }
     </div>
 }
 export default HomePageCard
