05d65236c8689a7e0f0694d0dd1a8840ff30c6e3
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-26 20:33:52 +0530

    feat: introduce generateMnemonic function which generate mnemonic and returns it

---
+++ src/utils/generateMnemonic.ts
@@ -0,0 +1,5 @@
+export const generateMnemonics = ():string=>{
+    // we need to decide which one they want is it 128 or 256, 12 words or 24 words
+    const mnemonic = generateMnemonics()
+    return mnemonic
+}
