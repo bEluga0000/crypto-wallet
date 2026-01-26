eb824456421e032f30b78d9f275d70fdc16f9e80
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-26 20:47:14 +0530

    feat: store the mnemonic in the localstorage as soon as you going to create

--- src/utils/generateMnemonic.ts
+++ src/utils/generateMnemonic.ts
@@ -3,5 +3,6 @@ import { generateMnemonic } from "bip39"
 export const generateMnemonics = ():string=>{
     // we need to decide which one they want is it 128 or 256, 12 words or 24 words
     const mnemonic = generateMnemonic(128)
+    localStorage.setItem("mnemonic",mnemonic)
     return mnemonic
 }
