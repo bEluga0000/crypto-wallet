1db12957cf0770d9d43f48a2bbc5aa669edc60c9
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-26 20:35:11 +0530

    fix: use correct function from bip39 to generate generateMnemonics

--- src/utils/generateMnemonic.ts
+++ src/utils/generateMnemonic.ts
@@ -1,5 +1,7 @@
+import { generateMnemonic } from "bip39"
+
 export const generateMnemonics = ():string=>{
     // we need to decide which one they want is it 128 or 256, 12 words or 24 words
-    const mnemonic = generateMnemonics()
+    const mnemonic = generateMnemonic(128)
     return mnemonic
 }
