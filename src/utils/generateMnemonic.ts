4b65a6a93ba938ea48d60bb47a26055f40d3fc9b
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-26 22:27:22 +0530

    fix: add check for already existing mnemonic

--- src/utils/generateMnemonic.ts
+++ src/utils/generateMnemonic.ts
@@ -1,8 +1,11 @@
 import { generateMnemonic } from "bip39"
 
-export const generateMnemonics = ():string=>{
+export const generateMnemonics = (): string => {
     // we need to decide which one they want is it 128 or 256, 12 words or 24 words
-    const mnemonic = generateMnemonic(128)
-    localStorage.setItem("mnemonic",mnemonic)
+    let mnemonic = localStorage.getItem("mnemonic")
+    if (mnemonic)
+        return mnemonic
+    mnemonic = generateMnemonic(128)
+    localStorage.setItem("mnemonic", mnemonic)
     return mnemonic
 }
