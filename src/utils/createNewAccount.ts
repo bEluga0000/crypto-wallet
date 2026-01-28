0b8cee43f2c910f163feaae98cfc993066ea45e8
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-28 23:07:11 +0530

    feat:finish solana account creation

--- src/utils/createNewAccount.ts
+++ src/utils/createNewAccount.ts
@@ -4,41 +4,38 @@ import { STORAGE_KEYS } from "@/constants/storageKeys";
 import { solanaKeyPairs } from "./generateKeyPairs/solanaKeyPairs";
 import { AccountSchema } from "@/constants/accounts";
 
-// [Log] Create Account: – {walletSource: "TRADING", chain: "solana", accountName: "8217346410"} (node_modules_next_dist_f3530cac._.js, line 2298)
 export const createNewPublicPrivateKey = (input: AddAccountFormSchema) => {
-    // check do we have data in the local storage
-    const accounts = localStorage.getItem(STORAGE_KEYS.ACCOUNTS)
-    if (!accounts)
-        return createNewAccount(input)
-    // if then create a new wallet with index zero
-
-}
-
-const createNewAccount = (input: AddAccountFormSchema) => {
-    // Ensure input.chain is of type COIN_TYPES_KEYS
-    const coinKey = input.chain as COIN_TYPES_KEYS;
-    const derivationPath = getDerivationPath(coinKey, 0);
-    const raw = localStorage.getItem(STORAGE_KEYS.ACCOUNTS);
+    const raw = localStorage.getItem(STORAGE_KEYS.ACCOUNTS)
+    console.log(raw)
     const accounts: AccountSchema[] = raw ? JSON.parse(raw) : [];
-    let privateKey = null
-    let publicKey = null
-    if (coinKey == "solana") {
+    console.log(accounts)
+    const filteredAccounts = accounts
+        .filter(acc => acc.coin == input.chain)
+    .sort((a, b) => b.index - a.index);
+    console.log(filteredAccounts)
+    const coinKey = input.chain as COIN_TYPES_KEYS;
+    const newIndex = filteredAccounts.length > 0 ? filteredAccounts[0].index + 1 : 0
+    const derivationPath = getDerivationPath(
+        coinKey, 
+        newIndex
+    );
+    let privateKey = null;
+    let publicKey = null;
+    if (coinKey === "solana") {
         const keypairs = solanaKeyPairs(derivationPath)
         privateKey = keypairs.privateKey
         publicKey = keypairs.publicKey
     }
-
     if (privateKey && publicKey)
         accounts.push({
-            index: 0,
+            index: newIndex,
             type: input.walletSource,
             coin: input.chain,
             publicKey: publicKey,
             privateKey: privateKey,
             accountName: input.accountName
-
         })
-    localStorage.setItem(STORAGE_KEYS.ACCOUNTS,JSON.stringify(accounts))
+    localStorage.setItem(STORAGE_KEYS.ACCOUNTS, JSON.stringify(accounts))
     return !!publicKey && !!privateKey
 }
 
