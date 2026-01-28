c15f3bdae272c30fb3af0c7f051fe81f7ca6d1da
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-28 15:09:17 +0530

    feat: create a functino which return solan public and private key

--- src/utils/generateKeyPairs/solanaKeyPairs.ts
+++ src/utils/generateKeyPairs/solanaKeyPairs.ts
@@ -1,6 +1,9 @@
 import { STORAGE_KEYS } from "@/constants/storageKeys"
+import { Keypair } from "@solana/web3.js"
 import { mnemonicToSeedSync } from "bip39"
 import { derivePath } from "ed25519-hd-key"
+import nacl from "tweetnacl"
+import bs58 from "bs58";
 export const solanaKeyPairs = (derivationPath: string)=>
 {
     const mnemonic = localStorage.getItem(STORAGE_KEYS.MNEMONIC)
@@ -8,5 +11,11 @@ export const solanaKeyPairs = (derivationPath: string)=>
         return
     const seed = mnemonicToSeedSync(mnemonic)
     const derivedSeed = derivePath(derivationPath,seed.toString("hex")).key
-    
+    const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey
+    const publicKey = Keypair.fromSecretKey(secret).publicKey.toBase58()
+    const privateKey = bs58.encode(secret);
+    return{
+        publicKey,
+        privateKey
+    }
 }
