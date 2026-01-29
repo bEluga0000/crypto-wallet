ff4a473cccf95d7404f580eb5c494eed581a65c5
shreyas-wysbryx <shreyas@wysbryx.com>
2026-01-29 20:20:40 +0530

    feat: introduce a function to genereate bitcoin key pairs

--- src/utils/generateKeyPairs/bitcoinKeyPairs.ts
+++ src/utils/generateKeyPairs/bitcoinKeyPairs.ts
@@ -1,20 +1,33 @@
-import { STORAGE_KEYS } from "@/constants/storageKeys"
-import { mnemonicToSeedSync } from "bip39"
-import { ChainKeyPairsoutputSchema } from "./solanaKeyPairs"
-import { HDNodeWallet } from "ethers"
+import { STORAGE_KEYS } from "@/constants/storageKeys";
+import { mnemonicToSeedSync } from "bip39";
+import * as bitcoin from "bitcoinjs-lib";
+import * as ecc from "tiny-secp256k1";
+import * as bip32 from "bip32";
+import { ChainKeyPairsoutputSchema } from "./solanaKeyPairs";
 
-export const ethereumKeyPairs = (derivationPath: string): ChainKeyPairsoutputSchema => {
-    const mnemonic = localStorage.getItem(STORAGE_KEYS.MNEMONIC)
-    if (!mnemonic)
-        return {
-            publicKey: null,
-            privateKey: null
-        }
-    const seed = mnemonicToSeedSync(mnemonic)
-    const hdNode = HDNodeWallet.fromSeed(seed);
-    const wallet = hdNode.derivePath(derivationPath)
-    return {
-        publicKey:wallet.publicKey,
-        privateKey:wallet.privateKey
+bitcoin.initEccLib(ecc);
+const bip32Factory = bip32.BIP32Factory(ecc);
+
+export const bitcoinKeyPairs = (
+    derivationPath: string
+): ChainKeyPairsoutputSchema => {
+    const network = bitcoin.networks.bitcoin;
+    const mnemonic = localStorage.getItem(STORAGE_KEYS.MNEMONIC);
+
+    if (!mnemonic) {
+        return { publicKey: null, privateKey: null };
     }
-}
+
+    const seed = mnemonicToSeedSync(mnemonic);
+    const root = bip32Factory.fromSeed(seed, network);
+    const child = root.derivePath(derivationPath);
+
+    if (!child.privateKey || !child.publicKey) {
+        return { publicKey: null, privateKey: null };
+    }
+
+    return {
+        publicKey: Buffer.from(child.publicKey).toString("hex"),
+        privateKey: Buffer.from(child.privateKey).toString("hex"),
+    };
+};
