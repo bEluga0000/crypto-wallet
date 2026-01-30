2a2b7c9467c6cda3a4c3d30a71e835219879a010
bEluga0000 <shreyasas2975@email.com>
2026-01-30 12:10:18 +0530

    feat: upgrade import wallet state

--- src/store/importWallet.ts
+++ src/store/importWallet.ts
@@ -3,17 +3,22 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
 type ImportWalletState = {
-    walletName:string|null
-}
+  walletName: string | null;
+  setWalletName: (walletName: string | null) => void;
+  clearWalletName: () => void;
+};
 
 export const useImportWalletStore = create<ImportWalletState>()(
-    persist(
-        (set)=>({
-            walletName:null,
-            setWalletName:(walletName:string)=>set({walletName})
-        }),
-        {
-            name:STORAGE_KEYS.IMPORT_WALLET_NAME
-        }
-    )
-)
+  persist(
+    (set) => ({
+      walletName: null,
+
+      setWalletName: (walletName) => set({ walletName }),
+
+      clearWalletName: () => set({ walletName: null }),
+    }),
+    {
+      name: STORAGE_KEYS.IMPORT_WALLET_NAME,
+    }
+  )
+);
