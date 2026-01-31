860fae1c7f25b2126130c3af3152d8ac1667014a
bEluga0000 <shreyasas2975@email.com>
2026-01-31 23:14:54 +0530

    feat: go to portfolio if seed phrase already present

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
 import HomePage from "@/components/home";
+import { useMnemonicStore } from "@/store/mnemonic.store";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const mnemonic = useMnemonicStore(s => s.mnemonic)
+  const router = useRouter()
+  useEffect(() => {
+    if (mnemonic) {
+      router.replace("/portfolio");
+    }
+  }, [mnemonic, router]);
   return (
     <div>
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
