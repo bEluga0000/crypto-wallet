15c3f73325524a188d1a60dfeb02cc00a5e8ac02
bEluga0000 <shreyasas2975@email.com>
2026-02-01 01:54:04 +0530

    feat: no account found component

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -7,14 +7,14 @@ import CoinCard from "./ui/portfolio/coinCard";
 import { useEffect, useState } from "react";
 import AddNewAccountModal from "./ui/modals/addNewAccount";
 import { AccountSchema } from "@/constants/accounts";
-import { AccountTypeKey } from "@/constants/accountTypes";
+import { ACCOUNT_TYPES, AccountTypeKey } from "@/constants/accountTypes";
 import BalanceFilterSelect from "./ui/portfolio/accountDropDown";
 import { useAccountStore } from "@/store/accounts.store";
 
 export default function PortfolioPage() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const [openAddAccount, setOpenAddAccount] = useState(false)
-  const accounts = useAccountStore(s=>s.accounts)
+  const accounts = useAccountStore(s => s.accounts)
   const [acc, setAcc] = useState<string | null>(null);
   const [accountsOptions, setAccountOptions] = useState<string[]>([])
   const [selectWalletType, setSelectedWalletType] = useState<AccountTypeKey | "HARDWARE" | "STAKING">("MAIN")
@@ -57,14 +57,28 @@ export default function PortfolioPage() {
             walletName={selectWalletType}
           />
           <div className="max-w-5xl px-8 py-10 gap-3 flex flex-col">
-            <BalanceFilterSelect
-              value={acc ?? ""}
-              options={accountsOptions}
-              onChange={setAcc}
-              placeholder="No Account Found"
-            />
-            <BalanceCard />
+            {
+              acc && acc.length > 0 && <BalanceFilterSelect
+                value={acc ?? ""}
+                options={accountsOptions}
+                onChange={setAcc}
+                placeholder="No Account Found"
+              />
+            }
+            {/* <BalanceCard /> */}
             {/* Assets */}
+            {
+              (!filteredAccounts || filteredAccounts.length == 0) && <div className="flex flex-col items-center justify-center rounded-xl border border-dashed border-slate-300 dark:border-slate-700 bg-white dark:bg-[#16181d] p-10 text-center">
+                <h3 className="text-lg font-semibold">
+                  No accounts found
+                </h3>
+
+                <p className="mt-2 max-w-md text-sm text-slate-500">
+                  There are no accounts created under{" "}
+                  <span className="font-semibold">{ACCOUNT_TYPES[selectWalletType as AccountTypeKey].label}</span>.
+                </p>
+              </div>
+            }
             {
               filteredAccounts.length > 0 && <div className="space-y-4">
                 {filteredAccounts.map((c, ind) => {
@@ -74,7 +88,7 @@ export default function PortfolioPage() {
             }
 
             {/* Allocation */}
-            <div className="mt-12">
+            {/* <div className="mt-12">
               <h4 className="mb-4 text-xs font-bold uppercase tracking-widest text-slate-500">
                 Allocation
               </h4>
@@ -83,7 +97,7 @@ export default function PortfolioPage() {
                 <div className="h-full w-[36%] bg-blue-500" />
                 <div className="h-full w-[24%] bg-purple-500" />
               </div>
-            </div>
+            </div> */}
           </div>
         </main>
       </div>
