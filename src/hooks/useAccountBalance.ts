e3de0af2d85be84f299c0100283b06be063ea24d
bEluga0000 <shreyasas2975@email.com>
2026-01-31 12:34:10 +0530

    feat: make a hook supports all coin balances

---
+++ src/hooks/useAccountBalance.ts
@@ -0,0 +1,66 @@
+
+import { AccountSchema } from "@/constants/accounts";
+import { useBalanceStore } from "@/store/balance.store";
+import { getSolanaBalance } from "@/utils/balances/solanaBalance";
+import { useEffect, useState } from "react";
+
+export function useSolanaBalance(account: AccountSchema) {
+  const key = `${account.coin}:${account.publicKey}`
+  const balance = useBalanceStore((s) => s.balances[key]);
+  const setBalance = useBalanceStore((s) => s.setBalance);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!account.publicKey) return;
+
+    let mounted = true;
+
+    const loadBalance = async () => {
+      try {
+        setLoading(true);
+        setError(null)
+        let value = 0
+        switch (account.coin) {
+          case "solana": {
+            const res = await getSolanaBalance(account.publicKey)
+            value = res.sol
+            break
+          }
+          case "ethereum": {
+            // const res = await getEthereumBalance(account.publicKey);
+            // value = res.wei;
+            break;
+          }
+
+          case "bitcoin": {
+            // const res = await getBitcoinBalance(account.publicKey);
+            // value = res.sats;
+            break;
+          }
+          default:
+            return
+
+        }
+        if (mounted) setBalance(key, value);
+      } catch (e) {
+        if (mounted) setError("Failed to fetch balance");
+      } finally {
+        if (mounted) setLoading(false);
+      }
+    };
+
+    loadBalance();
+
+    return () => {
+      mounted = false;
+    };
+  }, [account.publicKey, account.coin,key,setBalance]);
+
+  return {
+    lamports: balance ?? 0,
+    loading,
+    error,
+  };
+}
